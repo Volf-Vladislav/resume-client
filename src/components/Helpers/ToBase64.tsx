1ef021b7ae7df19970c6fdd2aabf2e485571ab30
Vladislav Volf <therealmiyutreaz@gmail.com>
2023-12-01 21:13:38 +0300

    fix any type

--- src/components/Helpers/ToBase64.tsx
+++ src/components/Helpers/ToBase64.tsx
@@ -1,13 +1,18 @@
-const ToBase64 = (e:any) => {
+interface HTMLInputEvent extends Event {
+    target: HTMLInputElement & EventTarget
+}
+
+const ToBase64 = (e:HTMLInputEvent) => {
+    if (!e.target.files) return
+
     const file = e.target.files[0]
-    console.log(file)
     getBase64(file).then(base64 => {
         localStorage["fileBase64"] = base64
         console.debug("file stored", base64)
     })
 }
 
-const getBase64 = (file:any) => {
+const getBase64 = (file:File) => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader()
         reader.onload = () => resolve(reader.result)
